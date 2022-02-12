f794b4946708b0a6de0ae986d3bbd56ce8ae717a
pedaaal <pv.darya@gmail.com>
2022-02-12 12:05:19 +0300

    Задание №3

---
+++ zadanie3.js
@@ -0,0 +1,11 @@
+//Напишите функцию min(first, second), которая возвращает меньшее из чисел a и b
+function min() 
+{
+    if (a < b)
+        console.log(a);
+    else
+        console.log(b);
+}
+let a = 7;
+let b = 3;
+console.log(min(a,b));
