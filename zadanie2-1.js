73849bcb39de6532a0e5c1558b8040dfed89e2ba
pedaaal <pv.darya@gmail.com>
2022-03-14 10:51:22 +0300

    Добавление системы логирования

--- zadanie2-1.js
+++ zadanie2-1.js
@@ -1,15 +1,23 @@
 const http = require("http");
+
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
-     if (request.url === "/home" || request.url === "/") {
+    if (request.url === "/home" || request.url === "/") {
          response.write("<h2>Home</h2>");
-     } else if (request.url == "/about") {
+    } else if (request.url == "/about") {
          response.write("<h2>About</h2>");
-     } else if (request.url == "/contact") {
+    } else if (request.url == "/contact") {
          response.write("<h2>Contacts</h2>");
-     } else {
+    } else {
          response.write("<h2>Not found</h2>");
-     }
-     response.end();
+    }
+
+    console.log("Url: " + request.url);
+    console.log("Тип запроса: " + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    
+    response.end();
 }
+
 http.createServer(requestHandler).listen(3000);
