eab45aabc76541ccdc23ccec915c4f11cd3aab0c
pedaaal <pv.darya@gmail.com>
2022-09-10 11:01:10 +0300

    Практика 3.1

---
+++ html/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = requier('path');;
+const PORT = 3000;
+const ROOT = path.join(__dirname)
+
+express.static()
+
+const app = express();
+
+app.get('/', (req, es))
