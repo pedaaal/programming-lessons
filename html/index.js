62e4265aabbf506729a89c684e7f54d93be404c4
pedaaal <pv.darya@gmail.com>
2022-09-10 13:14:04 +0300

    всё заработало

--- html/index.js
+++ html/index.js
@@ -2,25 +2,21 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs').promises;
 const PORT = 3000;
+const HOST = 'localhost';
 const ROOT = path.join(__dirname);
 
 const app = express();
 
-app.use('/static', express.static(path.join(ROOT, 'static')));
+app.use('/static', express.static('static'));
 
-app.get('/', (req, res, next) => {
-    console.log(req.ip);
-    fs.readFile(path.join(ROOT, 'index,html'))
-    .then(content => {
-        const name = 'Bob';
-        content = content.toString().replace('{name}', name);
-        res.send(content);
-    })
-    .catch(e => next());
+app.get("/:id",(req,res) =>{
+    let {id}=req.params;
+    console.log(id);
+    res.sendFile(path.join(ROOT,'файлы', `${id}.html`));
 });
 
 app.get('*', (req, res) => {
     res.send('404 not found');
-})
+});
 
-app.listen(PORT, () => console.log('Server is running...'));
+app.listen(PORT, () => console.log(`Server is running on http://${HOST}:${PORT}`));
