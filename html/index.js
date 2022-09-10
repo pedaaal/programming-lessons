a5a3f093a57351261a4ccf6d4769adb0a1db2d7d
pedaaal <pv.darya@gmail.com>
2022-09-10 11:29:37 +0300

    добавление кота и создание сервера

--- html/index.js
+++ html/index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-const path = requier('path');;
+const path = require('path');
+const fs = require('fs').promises;
 const PORT = 3000;
-const ROOT = path.join(__dirname)
-
-express.static()
+const ROOT = path.join(__dirname);
 
 const app = express();
 
-app.get('/', (req, es))
+app.use('/static', express.static(path.join(ROOT, 'static')));
+
+app.get('/', (req, res, next) => {
+    console.log(req.ip);
+    fs.readFile(path.join(ROOT, 'index,html'))
+    .then(content => {
+        const name = 'Bob';
+        content = content.toString().replace('{name}', name);
+        res.send(content);
+    })
+    .catch(e => next());
+});
+
+app.get('*', (req, res) => {
+    res.send('404 not found');
+})
+
+app.listen(PORT, () => console.log('Server is running...'));
